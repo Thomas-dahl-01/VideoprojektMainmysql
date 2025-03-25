176170a23619d74860b20582467b2a1e315ad58f
Thomas-dahl-01 <thom0002@stud.kea.dk>
2025-03-25 10:47:08 +0100

    password

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -6,13 +6,13 @@ CREATE DATABASE BilProjekt;
 USE BilProjekt;
 
 CREATE TABLE cars (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      brand VARCHAR(100) NOT NULL,
-                      modelyear INT NOT NULL,
-                      type VARCHAR(100) NOT NULL,
-                      colour VARCHAR(50) NOT NULL,
-                      licenseplate VARCHAR(50) NOT NULL,
-                      img VARCHAR(255)
+id INT AUTO_INCREMENT PRIMARY KEY,
+brand VARCHAR(100) NOT NULL,
+modelyear INT NOT NULL,
+type VARCHAR(100) NOT NULL,
+colour VARCHAR(50) NOT NULL,
+licenseplate VARCHAR(50) NOT NULL,
+img VARCHAR(255)
 );
 
 
